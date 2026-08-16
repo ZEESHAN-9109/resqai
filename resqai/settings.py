9ed3e74e204a47fa03e0a58b1379b3659ede4407
ZEESHAN AHMED <zeahmed15432@gmail.com>
2026-08-16 17:23:50 +0530

    fix static files for production

--- resqai/settings.py
+++ resqai/settings.py
@@ -93,7 +93,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [BASE_DIR /"resqai" /"static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STORAGES = {
     "default": {"BACKEND": "django.core.files.storage.FileSystemStorage"},
